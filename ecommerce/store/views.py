724cf46cbe76e13f045683713e97c04567971d1d
Rediet Bekele <150306071+Nanioal@users.noreply.github.com>
2024-12-18 19:07:10 +0000

    Implemented category endpoint with token authentication

---
+++ ecommerce/store/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+# store/views.py
+
+from rest_framework import generics, permissions
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
+
+class CategoryListCreateView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class ProductListCreateView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+# Create your views here.
